062115ae8f7a5caf9b54a83e85be406ab627adef
Claudio Matheus <claudiomatheus055@gmail.com>
2024-05-22 16:13:39 -0300

    Remover .vs/ do controle de versão e adicioná-lo ao .gitignore

---
+++ xadrex-console/Program.cs
@@ -0,0 +1,21 @@
+﻿using tabuleiro;
+using System;
+using xadrez;
+using System.Net.Http.Headers;
+
+namespace xadrex_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+
+            Console.WriteLine(pos);
+
+           
+           Console.ReadLine();
+
+        }
+    }
+}
---
+++ xadrex-console/Tela.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using tabuleiro;
+
+
+namespace xadrex_console
+{
+     class Tela
+    {
+        public static void imprimirTabuleiro(Tabuleiro tab)
+        {
+            for (int i = 0; i<tab.linhas; i++)
+            {
+                Console.Write(8 - i + " ");
+                for(int j = 0; j<tab.colunas; j++)
+                {
+                    if (tab.peca(i, j) == null)
+                    {
+                        Console.Write("- ");
+                    }
+                    else
+                    {
+                        Console.Write(tab.peca(i, j) + " ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+                
+        }
+    }
+}
